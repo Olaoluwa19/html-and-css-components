056b537aa57cfbf05646f1f3bcbc887febd42d02
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-06-19 03:07:16 +0100

    added show and close menu logic and also renamed index.js to main.js

---
+++ 11_responsive_portfolio_website/assets/js/main.js
@@ -0,0 +1,18 @@
+// SHOW MENUN: Y HIDDEN
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+// SHOW MENU
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// HIDE MENU
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
