213b1ea968e032350e03a49d241834886bd305dd
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-06-21 21:17:22 +0100

    added toggle navlink logic

--- 11_responsive_portfolio_website/assets/js/main.js
+++ 11_responsive_portfolio_website/assets/js/main.js
@@ -16,3 +16,13 @@ if (navClose) {
     navMenu.classList.remove("show-menu");
   });
 }
+
+// REMOVE MENU MOBILE(when links are clicked)
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // remove show-menu class when links are clicked
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
