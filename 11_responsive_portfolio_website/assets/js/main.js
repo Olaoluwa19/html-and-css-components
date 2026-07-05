c27a74f80c0eda81be30e4fd590314029e2ebde1
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-07-05 02:41:12 +0100

    added accordion functionality for the skills display

--- 11_responsive_portfolio_website/assets/js/main.js
+++ 11_responsive_portfolio_website/assets/js/main.js
@@ -26,3 +26,45 @@ const linkAction = () => {
   navMenu.classList.remove("show-menu");
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// SKILLS ACCORDION
+// const skillsContent = document.getElementsByClassName("skills__content"),
+//   skillsHeader = document.querySelectorAll(".skills__header");
+
+// function toggleSkills() {
+//   let itemClass = this.parentNode.className;
+
+//   // Close all
+//   for (let i = 0; i < skillsContent.length; i++) {
+//     skillsContent[i].classList.remove("skills__open");
+//     skillsContent[i].classList.add("skills__close");
+//   }
+
+//   // Open the clicked one
+//   if (itemClass === "skills__content skills__close") {
+//     this.parentNode.classList.remove("skills__close");
+//     this.parentNode.classList.add("skills__open");
+//   }
+// }
+
+// skillsHeader.forEach((el) => {
+//   el.addEventListener("click", toggleSkills);
+// });
+
+const skillsHeader = document.querySelectorAll(".skills__header");
+
+skillsHeader.forEach((header) => {
+  header.addEventListener("click", function () {
+    const parent = this.parentNode; // this = clicked header
+
+    // Close all others
+    document.querySelectorAll(".skills__content").forEach((content) => {
+      content.classList.remove("skills__open");
+      content.classList.add("skills__close");
+    });
+
+    // Open current
+    parent.classList.toggle("skills__open");
+    parent.classList.toggle("skills__close");
+  });
+});
